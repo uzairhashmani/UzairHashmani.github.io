eaffd31e565520633e0ebe2fb48663ac87d4f89c
Uzair Hashmani <uz.hashmani@gmail.com>
2025-01-20 04:11:07 +0500

    Login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black-dark': '#00000050',
+        'dull-white': '#FFFFFFB3',
+        'white-light': '#FFFFFF30',
+        'white-medium': '#FFFFFF40',
+        'neon-blue': '#2FB8FF',
+      }
+    },
   },
   plugins: [],
 }
